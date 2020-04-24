1ef4bf440f6b0c07c28bc129dec5ee8ec0cf6172
Ademar Gonçalves das Candeias Junior <1939890@cefetes.br>
2020-04-23 22:18:49 -0300

    update fields

--- almox_control/materiais/admin.py
+++ almox_control/materiais/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
+from materiais.models.cadastro import Fornecedor, NotaEmpenho, Pagamento
 
-from materiais.models.cadastro import Fornecedor, NotaEmpenho
 
-admin.site.register(Fornecedor)
-admin.site.register(NotaEmpenho)
+class FornecedorAdmin(admin.ModelAdmin):
+    model = Fornecedor
+    list_display = ['cnpj','razao', 'estado', 'email',
+                    'telefone']
+    list_filter = ['razao', 'estado']
+    search_fields = ['razao']
+
+admin.site.register(Fornecedor, FornecedorAdmin)
+
+
+class NotaEmpenhoAdmin(admin.ModelAdmin):
+    model = NotaEmpenho
+    list_display = ['ne','data', 'fornecedor', 'tipo']
+    list_filter = ['ne', 'data', 'fornecedor']
+    search_fields = ['data', 'ne', 'fornecedor']
+
+admin.site.register(NotaEmpenho, NotaEmpenhoAdmin)
+
+class PagamentoAdmin(admin.ModelAdmin):
+    model = Pagamento
+    list_display = ['ne','valorPago', 'dataPagamento', 'notaFiscal', 'credor', 'processo']
+    list_filter = ['ne', 'dataPagamento', 'processo']
+    search_fields = ['ne', 'processo']
+
+admin.site.register(Pagamento, PagamentoAdmin)
--- almox_control/materiais/models/cadastro.py
+++ almox_control/materiais/models/cadastro.py
@@ -8,17 +8,38 @@ CHOICES_TIPO = (
     )
 
 class Fornecedor(models.Model):
-    cnpj = BRCNPJField(verbose_name="CNPJ")
+    cnpj = BRCNPJField(verbose_name='CNPJ', max_length=14)
     razao = models.CharField(verbose_name="Razão Social", max_length=100)
     estado = BRStateField(verbose_name="Estado")
     email = models.EmailField(verbose_name="E-mail")
     telefone = models.CharField(verbose_name="Telefone", max_length=15)
 
     def __str__(self):
-        return "{0}".format(self.cnpj)
+        return "{0}".format(self.razao)
 
 
 class NotaEmpenho(models.Model):
+    ne = models.CharField(verbose_name='NE', max_length=16)
+    data = models.DateField(verbose_name='Data', auto_now_add=True)
+    valor = models.FloatField(verbose_name='Valor')
+    saldo = models.FloatField(verbose_name='Saldo')
+    descricao = models.CharField(verbose_name='Descrição', max_length=16)
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, related_name='notas')
-
     tipo = models.IntegerField(choices=CHOICES_TIPO)
+    requerente = models.CharField(verbose_name='Requerente', max_length=4)
+    observacao = models.CharField(verbose_name='Observacao', max_length=255)
+    prazoEntrega = models.CharField(verbose_name='PrazoEntrega', max_length=20)
+
+    def __str__(self):
+        return self.ne
+
+class Pagamento(models.Model):
+    ne = models.ForeignKey(NotaEmpenho, on_delete=models.CASCADE)
+    valorPago = models.FloatField(verbose_name='ValorPago')
+    dataPagamento = models.DateField(verbose_name='DataPag', auto_now_add=True)
+    notaFiscal = models.IntegerField(verbose_name='NotaFiscal') 
+    credor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, related_name='pagamentos')
+    processo = models.CharField(verbose_name='Processo', max_length=20)
+
+    def __str__(self):
+        return "{0}".format(self.processo)
